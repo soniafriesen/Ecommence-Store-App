dbb509bda3af94cf4d34a9a71debdf29b9baa8ea
soniafriesen <58013701+soniafriesen@users.noreply.github.com>
2020-06-16 19:40:46 -0400

    Add files via upload

---
+++ Server Side/helpers/OrderDetailsHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Casestudy.Helpers
+{
+    public class OrderDetailsHelper
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public string ProductId { get; set; }
+        public string Description { get; set; }
+        public string ProductName { get; set; }
+        public float CostMSRP { get; set; }
+        public int QTYSold { get; set; }
+        public int QTYOrdered { get; set; }
+        public int QTYBacked { get; set; }
+        public int QTY { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Tax {get; set; }
+        public decimal OrderTotal { get; set; }
+        public string DateCreated { get; set; }
+    }
+}
---
+++ Server Side/helpers/OrderHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Casestudy.Helpers
+{
+    public class OrderHelper
+    {
+        public string email { get; set; }
+        public OrderSelectionHelper[] selections { get; set; }
+    }
+}
